1e58774d95d6ecaac26c936561e9c880c9774d52
Dave136 <davejs136@gmail.com>
2022-05-09 15:20:05 -0400

    add: board-card

---
+++ src/components/BoardCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-link
+    class="w-2/6 lg:w-[250px] bg-cyan-700 shadow-md shadow-gray-700 box-border rounded-sm text-light-600 my-2 mx-6 p-4 text-left no-underline transition-all w-full h-[100px] hover:(bg-cyan-600 font-bold)"
+    :to="{
+      name: 'board', params: { id, name }
+    }">
+    <span class="text-xl font-bold">{{ name }}</span>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+type Props = {
+  id: number;
+  name: string;
+}
+
+defineProps<Props>();
+</script>
